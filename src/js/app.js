eca86d3fba0cee8cdf223e10e6f30765328f4e4f
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2024-06-26 18:07:46 +0200

    frontend terminado

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton')
 
     botonDarkMode.addEventListener('click', function() {
